c6bfacc7b61441b6137ed7c720e52883596313f5
Damian Podraza <damianpdr@gmail.com>
2019-10-29 18:11:40 +0100

    refactor alignment

--- src/components/Category.js
+++ src/components/Category.js
@@ -9,11 +9,11 @@ const Category = ({ categoryName, description, details, imgName  }) => {
      <div className="container" style={{marginBottom: '50px', marginTop: '50px', marginRight: '-48px'}}>
        <div className="columns">
          <div className="column is-narrow is-offset-1">
-          <h1 style={{fontSize: '40px', fontWeight: 'bold', marginTop: '150px'}}>{categoryName}</h1>
-          <p style={{fontStyle: 'italic', fontSize: '14px'}}>
+          <h1 style={{fontSize: '40px', fontWeight: 'bold', marginTop: '150px', textAlign: 'end'}}>{categoryName}</h1>
+          <p style={{fontStyle: 'italic', fontSize: '14px', textAlign: 'end'}}>
           {description}
           </p>
-          <p style={{opacity: '0.6', fontSize: '12px', marginTop: '5px', fontFamily: 'Lato', fontWeight: '300'}}>
+          <p style={{opacity: '0.6', fontSize: '12px', marginTop: '5px', fontFamily: 'Lato', fontWeight: '300', textAlign: 'end'}}>
           {details}
           </p>
          </div>
