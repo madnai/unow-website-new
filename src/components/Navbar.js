07fed13fffd40a5a7ab1b3f8f7012b8241393af8
unknown <damianpdr@gmail.com>
2019-11-17 14:02:32 +0200

    change logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from '../img/logo.png';
+import logo from '../img/logo2.png';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile
+  MobileView
 } from "react-device-detect";
 
 
@@ -33,97 +31,7 @@ const NavbarComponent = class extends React.Component {
     this.setState({ isClient: true })
   }
   
-
-  // toggleHamburger = () => {
-  //   // toggle the active boolean in the state
-  //   this.setState(
-  //     {
-  //       active: !this.state.active,
-  //     },
-  //     // after state has been updated,
-  //     () => {
-  //       // set the class in state for the navbar accordingly
-  //       this.state.active
-  //         ? this.setState({
-  //             navBarActiveClass: 'is-active',
-  //           })
-  //         : this.setState({
-  //             navBarActiveClass: '',
-  //           })
-  //     }
-  //   )
-  // }
 render() {
-    // if(isBrowser) {
-    //   return (
-    //     <Navbar  style={{marginTop: '30px', fontFamily: 'Lato'}}>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //       <Navbar.Collapse id="basic-navbar-nav">
-    //           <div className="abs">
-    //             <Nav.Item>
-    //               <Link to="/">
-    //                 <img src={logo} alt="UNOWbrowser" width='200px;' />
-    //               </Link>
-    //             </Nav.Item>
-    //           </div>
-    //         <div className='navbar-nav ml-auto' style={{marginRight: '15%'}}>
-    //           <Nav.Item>
-    //             <Nav.Link style={{fontSize: '13px', fontWeight: '700', letterSpacing: '0.15em', color: 'black'}}>
-    //               <ExactNavLink to="/">HOME</ExactNavLink>
-    //             </Nav.Link>
-    //           </Nav.Item>
-    //           <Nav.Item>
-    //             <Nav.Link style={{fontSize: '13px', fontWeight: '700', letterSpacing: '0.15em', color: 'black'}}>
-    //               <ExactNavLink to="/onas">O NAS</ExactNavLink>
-    //             </Nav.Link>
-    //           </Nav.Item>
-    //           <Nav.Item>
-    //             <Nav.Link style={{fontSize: '13px', fontWeight: '700', letterSpacing: '0.15em', color: 'black'}}>
-    //               <ExactNavLink to="/warsztaty">WARSZTATY</ExactNavLink>
-    //             </Nav.Link>
-    //           </Nav.Item>
-    //           <Nav.Item>
-    //             <Nav.Link style={{fontSize: '13px', fontWeight: '700', letterSpacing: '0.15em', color: 'black', paddingRight: '0px'}}>
-    //               <ExactNavLink to="/planner">SKLEP</ExactNavLink>
-    //             </Nav.Link>
-    //           </Nav.Item>
-    //         </div>
-    //       </Navbar.Collapse>
-    //     </Navbar>
-    //   )
-    // };
-    // if(isMobile) {
-    //   return (
-    //     <Navbar collapseOnSelect expand="sm" style={{marginTop: '30px', fontFamily: 'Lato'}}>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //       <Navbar.Brand className="abs" href="#home"><Link to="/"><img src={logo} alt="UNOWmobile" width='200px;' /></Link></Navbar.Brand>
-    //       <Navbar.Collapse id="basic-navbar-nav">
-    //         <div className='navbar-nav ml-auto' style={{marginRight: '15%'}}>
-    //           <Nav.Item>
-    //             <Nav.Link style={{fontSize: '13px', fontWeight: '700', letterSpacing: '0.15em', color: 'black'}}>
-    //               <ExactNavLink to="/">HOME</ExactNavLink>
-    //             </Nav.Link>
-    //           </Nav.Item>
-    //           <Nav.Item>
-    //             <Nav.Link style={{fontSize: '13px', fontWeight: '700', letterSpacing: '0.15em', color: 'black'}}>
-    //               <ExactNavLink to="/onas">O NAS</ExactNavLink>
-    //             </Nav.Link>
-    //           </Nav.Item>
-    //           <Nav.Item>
-    //             <Nav.Link style={{fontSize: '13px', fontWeight: '700', letterSpacing: '0.15em', color: 'black'}}>
-    //               <ExactNavLink to="/warsztaty">WARSZTATY</ExactNavLink>
-    //             </Nav.Link>
-    //           </Nav.Item>
-    //           <Nav.Item>
-    //             <Nav.Link style={{fontSize: '13px', fontWeight: '700', letterSpacing: '0.15em', color: 'black', paddingRight: '0px'}}>
-    //               <ExactNavLink to="/planner">SKLEP</ExactNavLink>
-    //             </Nav.Link>
-    //           </Nav.Item>
-    //         </div>
-    //       </Navbar.Collapse>
-    //     </Navbar>
-    //   )
-    // }
     return (
       <React.Fragment key={this.state.isClient}>
       <BrowserView>
@@ -133,7 +41,7 @@ render() {
               <div className="abs">
                 <Nav.Item>
                   <Link to="/">
-                    <img src={logo} alt="UNOWbrowser" width='200px;' />
+                    <img src={logo} alt="UNOWbrowser" width='120px;' />
                   </Link>
                 </Nav.Item>
               </div>
@@ -165,7 +73,7 @@ render() {
       <MobileView>
         <Navbar collapseOnSelect expand="sm" style={{marginTop: '30px', fontFamily: 'Lato'}}>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Brand className="abs" href="#home"><Link to="/"><img src={logo} alt="UNOWmobile" width='200px;' /></Link></Navbar.Brand>
+          <Navbar.Brand className="abs" href="#home"><Link to="/"><img src={logo} alt="UNOWmobile" width='120px;' /></Link></Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
             <div className='navbar-nav ml-auto' style={{marginRight: '15%'}}>
               <Nav.Item>
