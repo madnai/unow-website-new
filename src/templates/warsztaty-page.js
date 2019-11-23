381fd6731756461f2925c227103524304c499ecd
unknown <damianpdr@gmail.com>
2019-11-23 15:56:23 +0200

    refactor

--- src/templates/warsztaty-page.js
+++ src/templates/warsztaty-page.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import styled from 'styled-components'
-import charytatywneImg from '../../static/img/char.png';
-import charytatywneImgHover from '../../static/img/char_hover.png';
-import indywidualneImg from '../../static/img/ind.png';
-import indywidualneImgHover from '../../static/img/ind_hover.png';
+import charytatywneImg from '../../static/img/ch.png';
+import charytatywneImgHover from '../../static/img/ch_h.png';
+import indywidualneImg from '../../static/img/in.png';
+import indywidualneImgHover from '../../static/img/in_h.png';
 import HoverImage from '../components//HoverImage';
 import {
   BrowserView,
@@ -39,11 +39,11 @@ const WarsztatyPage = ({data}) => {
         <Layout>
             <div style={{marginBottom: '50px'}}>
               <div className="columns" style={{marginTop: '70px'}}>
-                <div className="column  " style={{padding: '0px 20px 20px 0px'}}>
-                  <Link to="/warsztaty/charytatywne"><HoverImage src={charytatywneImg} hoverSrc={charytatywneImgHover} width='100%' /></Link>
+                <div className="column " style={{padding: '0px 20px 20px 0px'}}>
+                  <Link to="/warsztaty/charytatywne"><HoverImage src={charytatywneImg} hoverSrc={charytatywneImgHover} /></Link>
                 </div>
                 <div className="column " style={{padding: '0px'}}>
-                  <Link to="/warsztaty/indywidualne"><HoverImage src={indywidualneImg}  hoverSrc={indywidualneImgHover} width='100%' /></Link>
+                  <Link to="/warsztaty/indywidualne"><HoverImage src={indywidualneImg}  hoverSrc={indywidualneImgHover}  /></Link>
                 </div>
               </div>
             </div>
