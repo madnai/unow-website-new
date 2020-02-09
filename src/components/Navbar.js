75a2427165043958e231ce16a6f21ed746c9ee75
Damian Poddraza <damianpoddraza@MacBook-Pro-Damian.local>
2020-02-09 22:07:20 +0100

    change start color and add img for udream

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -68,12 +68,12 @@ render() {
               </Nav.Item>
               <Nav.Item>
                 <span class="nav-link" style={{fontSize: '12px', fontWeight: '700', letterSpacing: '0.15em', color: 'black', paddingRight: '0px'}}>
-                  <a href="https://relaxed-yalow-4ffa99.netlify.com/">UDREAM</a>
+                  <ExactNavLink to="/udream">UDREAM</ExactNavLink>
                 </span>
               </Nav.Item>
               <Nav.Item>
                 <Nav.Link style={{fontSize: '12px', fontWeight: '700', letterSpacing: '0.15em', color: 'black', paddingRight: '0px'}}>
-                  <ExactNavLink to="/start">START</ExactNavLink>
+                  <ExactNavLink to="/start" style={{color: '#D80029'}}>START</ExactNavLink>
                 </Nav.Link>
               </Nav.Item>
             </div>
---
+++ src/pages/udream/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import {
+  BrowserView,
+  MobileView
+} from "react-device-detect";
+
+export default () => {
+  return (
+    <>
+    <BrowserView>
+      <Layout>
+        <section>
+          <div class="container" style={{marginBottom: '50px', paddingLeft: '100px', paddingRight: '100px'}}>
+            <img src="/img/app.png" style={{marginTop: '3%'}} width="100%" align="center"></img>
+          </div>
+        </section>
+      </Layout>
+    </BrowserView>
+    <MobileView>
+      <Layout>
+          <div class="container">
+            <img src="/img/app.png" style={{marginTop: '3%'}} width="100%" align="center"></img>
+          </div>
+      </Layout>
+    </MobileView>
+    </>
+  )
+}
