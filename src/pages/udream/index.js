86a673377139fe09f20d5cdc8cced012a3d9a8c9
unknown <damianpdr@gmail.com>
2020-02-15 23:56:05 +0200

    Revert "add new pics and newsletter"
    This reverts commit efd5f66a4c8a2e7ea17c8fa33e0919662b590910.

--- src/pages/udream/index.js
+++ src/pages/udream/index.js
@@ -4,13 +4,8 @@ import {
   BrowserView,
   MobileView
 } from "react-device-detect";
-import MailchimpSubscribe from "react-mailchimp-subscribe"
 
 export default () => {
-  const url = "//unow.us4.list-manage.com/subscribe/post?u=f6224231d9d0990931893424f&amp;id=b4f5d178e6";
-  const SimpleForm = () => <MailchimpSubscribe message="asdasd"  url={url}/>
-
-  
   return (
     <>
     <BrowserView>
@@ -19,12 +14,10 @@ export default () => {
           <div class="container" style={{marginBottom: '150px', paddingLeft: '100px', paddingRight: '100px'}}>
             <img src="/img/app1.png" style={{marginTop: '3%'}} width="100%" align="center"></img>
             <div class="columns is-centered">
-            <MailchimpSubscribe message="asdasd"  url={url}/>
             </div>
+
           </div>
-          
         </section>
-        
       </Layout>
     </BrowserView>
     <MobileView>
