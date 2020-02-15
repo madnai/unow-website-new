9693f2e195f004567bbc866b82b16fd518ab4bd0
unknown <damianpdr@gmail.com>
2020-02-15 21:32:43 +0200

    fix mobile podcast page

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from 'gatsby'
 import ReactAudioPlayer from 'react-audio-player';
 import accents from 'remove-accents';
+import {
+  BrowserView,
+  MobileView, isBrowser
+} from "react-device-detect";
 
 const Episode = ({
   title,
@@ -18,21 +22,23 @@ const Episode = ({
 
   const title2 = accents(title)
   const par = title2.replace(/\s+/g, '-');
-  const cos = par.replace(/\?/g,'')
+  const titleFinal = par.replace(/\?/g,'')
 
   return (
     <div className="box" style={{marginBottom: '20px', marginTop: '20px'}}>
       <article className="media">
-        <div className="media-left">
-          <figure className="image is-128x128" style={{width: '128px'}}>
+        <div className="media-left" style={{marginBottom: '5px'}}>
+          {isBrowser ? (<figure className="image is-128x128" style={{width: '128px'}}>
             <img src={cover} />
-          </figure>
+          </figure>) : (<figure className="image" style={{width: '100%'}}>
+            <img src={cover} />
+          </figure>)
+          }
         </div>
         <div className="media-content">
           <div className="content">
             <p>
-              <Link to={`start/${cos}`}><h3 className="title">{`${episode}. ${title}`}</h3> </Link>
-          
+              <Link to={`start/${titleFinal}`}><h3 className="title">{`${episode}. ${title}`}</h3> </Link>
               <div dangerouslySetInnerHTML={{ __html: content.substring(0, 250) }} />
             </p>
           </div>
