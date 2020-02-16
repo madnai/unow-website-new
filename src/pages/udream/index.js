6903888bdcc973dfc0cb28a4d61aa0a2ffcd5377
unknown <damianpdr@gmail.com>
2020-02-16 21:55:13 +0200

    added newsletter

--- src/pages/udream/index.js
+++ src/pages/udream/index.js
@@ -4,6 +4,7 @@ import {
   BrowserView,
   MobileView
 } from "react-device-detect";
+import Mailchimp from 'react-mailchimp-form'
 
 export default () => {
   return (
@@ -14,8 +15,31 @@ export default () => {
           <div class="container" style={{marginBottom: '150px', paddingLeft: '100px', paddingRight: '100px'}}>
             <img src="/img/app2.png" style={{marginTop: '3%'}} width="100%" align="center"></img>
             <div class="columns is-centered">
-            </div>
-
+            
+            <Mailchimp
+              action='https://unow.us4.list-manage.com/subscribe/post?u=f6224231d9d0990931893424f&amp;id=b4f5d178e6'
+              fields={[
+                {
+                  name: 'EMAIL',
+                  placeholder: 'Email',
+                  type: 'email',
+                  required: true
+                }
+                
+              ]}
+              messages = {
+                {
+                  sending: "Wysyłam...",
+                  success: "W ciągu 24 godzin odezwiemy się do Ciebie!",
+                  error: "Niespodziewany błąd.",
+                  empty: "Musisz podać email.",
+                  duplicate: "Już mamy twój email",
+                  button: "Chcę!"
+                }
+              }
+              className='nnn'
+              />
+              </div>
           </div>
         </section>
       </Layout>
