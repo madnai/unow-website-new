273a98815facc29a095c0a6d4b5755e8efb1a46d
unknown <damianpdr@gmail.com>
2020-02-19 20:53:17 +0200

    minor fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = class extends React.Component {
           <div className="container has-background-white has-text-black">
             <div className="columns is-centered is-mobile">
               <div className="column has-text-centered ">
-                <span style={{padding: '5px'}}><a href="mailto:info@unow.pl" target="_blank" rel="noopener noreferrer"><FaRegEnvelope /></a></span>
+                <span style={{padding: '5px'}}><a href="mailto:unow.pl@gmail.com" target="_blank" rel="noopener noreferrer"><FaRegEnvelope /></a></span>
                 <span style={{padding: '5px'}}><a href="https://www.facebook.com/unowpl/" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a></span>
                 <span style={{padding: '5px'}}><a href="https://www.instagram.com/unow.pl/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></span>
                 <span style={{padding: '5px'}}><a href="https://www.tiktok.com/@unow.pl" target="_blank" rel="noopener noreferrer"><img src="/img/tiktok.svg" alt="tiktok" width="15px"></img></a></span>
